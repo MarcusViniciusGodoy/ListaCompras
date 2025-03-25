0c8c4db427fac71c20f25b1edb437be519562c3d
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-03-25 09:00:48 -0300

    Modularização

--- index.js
+++ index.js
@@ -1,49 +1,12 @@
-const inputItem = document.getElementById("input-item");
+import { criarItemDaLista } from "./scripts/criarItemDaLista.js";
+
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
-let contador = 0;
 
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault();
-    if (inputItem.value == ""){
-        alert("Insira um item no campo!");
-        return;
-    }
-    const itemDaLista = document.createElement("li");
-    const containerItemDaLista = document.createElement("div");
-    containerItemDaLista.classList.add("lista-item-container");
-    const inputCheckBox = document.createElement("input");
-    inputCheckBox.type = "checkbox";
-    inputCheckBox.id = "checkbox-" + contador++;
-    const nomeItem = document.createElement("p");
-    nomeItem.innerText = inputItem.value;
-
-    inputCheckBox.addEventListener("click", function(){
-        if (inputCheckBox.checked){
-            nomeItem.style.textDecoration = "line-through";
-        }else{
-            nomeItem.style.textDecoration = "none";
-        }
-    });
-
-    containerItemDaLista.appendChild(inputCheckBox);
-    containerItemDaLista.appendChild(nomeItem);
-    itemDaLista.appendChild(containerItemDaLista);
-
-    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
-        weekday: "long"
-    });
-    const data = new Date().toLocaleDateString("pt-BR");
-    const hora = new Date().toLocaleTimeString("pr-BR", {
-        hour: "numeric",
-        minute: "numeric"
-    });
-    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
-    const itemData = document.createElement("p");
-    itemData.innerText = dataCompleta;
-    itemDaLista.appendChild(itemData);
+    const itemDaLista = criarItemDaLista();
     listaDeCompras.appendChild(itemDaLista);
-    
     verificarListaVazia();
 });
 
